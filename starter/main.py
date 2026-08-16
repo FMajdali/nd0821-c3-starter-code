477ff4a98af364c45cf7ee8812c041c034062d3e
FMajdali <faris.majdali@gmail.com>
2026-08-16 09:39:15 +0000

    modify main.py

--- starter/main.py
+++ starter/main.py
@@ -1,1 +1,26 @@
 # Put the code for your API here.
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.testclient import TestClient
+"""
+class TaggedItem(BaseModel):
+    name: str
+    tags: Union[str, list] 
+    item_id: int
+"""
+app = FastAPI()
+
+@app.get("/")
+async def greetings():
+    return "hello"
+
+"""
+@app.post("/items/")
+async def create_item(item: TaggedItem):
+    return item
+"""
+
+client = TestClient(app)
+r = client.get("/")
+assert r.status_code == 200
+print(r.json())
